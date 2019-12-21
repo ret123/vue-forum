78e10aefeb0cf17014792a90c558a8e145e78c31
ret123 <rathish83@gmail.com>
2019-12-21 13:12:55 +0530

    Initial commit

---
+++ client/src/components/Nav.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">Community</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    
+    
+  </div>
+</nav>
+</template>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+   <div class="jumbotron">
+  <h1 class="display-3">Welcome to Community!</h1>
+  <p class="lead">A fully open source forum. Made with 💚 by ret83 </p>
+  <hr class="my-4">
+  
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Sign up!</a>
+  </p>
+</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'home',
+ 
+}
+</script>
